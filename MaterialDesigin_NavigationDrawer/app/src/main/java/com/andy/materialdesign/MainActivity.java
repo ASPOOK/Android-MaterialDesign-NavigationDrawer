e00c8787d2ca62d5359b0909734c89c54724f9f1
Andy <yourswee@163.com>
2015-05-28 10:22:26 +0800

    add toolbar items

--- MaterialDesigin_NavigationDrawer/app/src/main/java/com/andy/materialdesign/MainActivity.java
+++ MaterialDesigin_NavigationDrawer/app/src/main/java/com/andy/materialdesign/MainActivity.java
@@ -1,17 +1,25 @@
 package com.andy.materialdesign;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class MainActivity extends ActionBarActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
 
